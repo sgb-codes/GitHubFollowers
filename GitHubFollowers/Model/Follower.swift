8afaee5b5f2401325bd49abf936b5d178c988e56
Simon Barrett <sbarrett765@gmail.com>
2020-10-17 15:58:35 -0700

    Add: Follower Model Struct

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Simon Barrett on 17/10/2020.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
