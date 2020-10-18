46017ab463a5e0c03a64ac60e4e2b9c5e4b2748f
Simon Barrett <sbarrett765@gmail.com>
2020-10-18 12:25:29 -0700

    Add: downloadImage to GFAvatarImageView
    Add a network call to download user avatar image

--- GitHubFollowers/CustomViews/GFAvatarImageView.swift
+++ GitHubFollowers/CustomViews/GFAvatarImageView.swift
@@ -10,7 +10,7 @@ import UIKit
 class GFAvatarImageView: UIImageView {
     
     let placeholderImage = UIImage(named: "avatar-placeholder")!
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -30,4 +30,24 @@ class GFAvatarImageView: UIImageView {
         translatesAutoresizingMaskIntoConstraints = false
     }
     
+    
+    func downloadImage(from urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] (data, response, error) in
+            guard let self = self else { return }
+            if error != nil { return }
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
+            guard let data = data else { return }
+            
+            guard let image = UIImage(data: data) else { return }
+            
+            DispatchQueue.main.async {
+                self.image = image
+            }
+            
+        }
+        
+        task.resume()
+    }
 }
