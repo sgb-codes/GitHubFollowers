c3945b53801a0f23c761879f70a6659c5ee53f04
Simon Barrett <sbarrett765@gmail.com>
2020-10-18 14:44:25 -0700

    Add: Search bar to FollowerListVC
    User can now search through followers

--- GitHubFollowers/Screens/FollowerListVC.swift
+++ GitHubFollowers/Screens/FollowerListVC.swift
@@ -14,9 +14,10 @@ class FollowerListVC: UIViewController {
     }
     
     var username: String!
-    var followers: [Follower]   = []
-    var page: Int               = 1
-    var hasMoreFollowers        = true
+    var followers: [Follower]           = []
+    var filteredFollowers: [Follower]   = []
+    var page: Int                       = 1
+    var hasMoreFollowers                = true
     
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
@@ -25,6 +26,7 @@ class FollowerListVC: UIViewController {
         super.viewDidLoad()
         configureCollectionView()
         configureViewController()
+        configureSearchController()
         getFollowers(username: username, page: page)
         configureDataSource()
         
@@ -51,6 +53,16 @@ class FollowerListVC: UIViewController {
         collectionView.backgroundColor  = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
+    
+    
+    func configureSearchController() {
+        let searchController                    = UISearchController()
+        searchController.searchResultsUpdater   = self
+        searchController.searchBar.delegate     = self
+        searchController.searchBar.placeholder  = "Search for a username"
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationItem.searchController         = searchController
+    }
 
     
     func getFollowers(username: String, page: Int) {
@@ -72,7 +84,7 @@ class FollowerListVC: UIViewController {
                     return
                 }
                 
-                self.updateData()
+                self.updateData(on: followers)
                 
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad stuff Happened", message: error.rawValue, buttonTitle: "Ok")
@@ -92,7 +104,7 @@ class FollowerListVC: UIViewController {
     }
     
     
-    func updateData() {
+    func updateData(on followers: [Follower]) {
         var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
         snapshot.appendSections([.main])
         snapshot.appendItems(followers)
@@ -115,4 +127,19 @@ extension FollowerListVC: UICollectionViewDelegate {
             getFollowers(username: username, page: page)
         }
     }
+}
+
+extension FollowerListVC: UISearchResultsUpdating, UISearchBarDelegate {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let filter = searchController.searchBar.text, !filter.isEmpty else { return }
+        
+        filteredFollowers = followers.filter { $0.login.lowercased().contains(filter.lowercased()) }
+        updateData(on: filteredFollowers)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+      updateData(on: followers)
+    }
+    
+    
 }
