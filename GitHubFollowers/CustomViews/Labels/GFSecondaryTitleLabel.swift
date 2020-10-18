bfd503fcade672b89f3dccb0f12c981ac6540e91
Simon Barrett <sbarrett765@gmail.com>
2020-10-18 16:29:10 -0700

    Add: GFSecondaryTitleLabel and GFUserInfoHeaderVC
    Custom Reusable classes. Both are empty at the moment

---
+++ GitHubFollowers/CustomViews/Labels/GFSecondaryTitleLabel.swift
@@ -0,0 +1,12 @@
+//
+//  GFSecondaryTitleLabel.swift
+//  GitHubFollowers
+//
+//  Created by Simon Barrett on 18/10/2020.
+//
+
+import UIKit
+
+class GFSecondaryTitleLabel: UILabel {
+
+}
---
+++ GitHubFollowers/CustomViews/ViewControllers/GFUserInfoHeaderVC.swift
@@ -0,0 +1,16 @@
+//
+//  GFUserInfoHeaderVC.swift
+//  GitHubFollowers
+//
+//  Created by Simon Barrett on 18/10/2020.
+//
+
+import UIKit
+
+class GFUserInfoHeaderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
