e11ae898386f0395c35266228ade01208083a745
Simon Barrett <sbarrett765@gmail.com>
2020-10-17 17:00:49 -0700

    Add: NetworkManager
    Add Network Manager Class and Get request from Github API
    
    Add func getFollowers to FollowerListVC

--- GitHubFollowers/Screens/FollowerListVC.swift
+++ GitHubFollowers/Screens/FollowerListVC.swift
@@ -16,6 +16,16 @@ class FollowerListVC: UIViewController {
 
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessage) in
+            guard let followers = followers else {
+                self.presentGFAlertOnMainThread(title: "Bad stuff Happened", message: errorMessage!, buttonTitle: "Ok")
+                return
+            }
+            
+            print(followers.count)
+            print(followers)
+        }
 
     }
     
