15238b61f86d59e0a1ebeb1d8048900ed604b52d
Simon Barrett <sbarrett765@gmail.com>
2020-10-19 16:27:02 -0700

    Add GFFollowerItemVC
    Customized VC to show users followers and following and button to take you to their followers list

---
+++ GitHubFollowers/CustomViews/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -0,0 +1,24 @@
+//
+//  GFFollowerItemVC.swift
+//  GitHubFollowers
+//
+//  Created by Simon Barrett on 19/10/2020.
+//
+
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureItems()
+    }
+    
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, with: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, with: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+}
