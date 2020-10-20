85fbe54820f9bcc10f1e8bddf202136db41d7255
Simon Barrett <sbarrett765@gmail.com>
2020-10-20 12:38:26 -0700

    Add: GFTabBarController
    Refactor tab bar setup from SceneDelegate to custom class

---
+++ GitHubFollowers/CustomViews/TabBarControllers/GFTabBarController.swift
@@ -0,0 +1,36 @@
+//
+//  GFTabBarController.swift
+//  GitHubFollowers
+//
+//  Created by Simon Barrett on 20/10/2020.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UITabBar.appearance().tintColor = .systemGreen
+        self.viewControllers            = [createSearchNC(), createFavoritesNC()]
+    }
+
+    
+    func createSearchNC() -> UINavigationController {
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    
+    func createFavoritesNC() -> UINavigationController {
+        let favoritesListVC = FavoritesListVC()
+        favoritesListVC.title = "Favorites"
+        favoritesListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesListVC)
+    }
+}
